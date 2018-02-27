55c1c33295cc5afc21e757d8671dba9442600a27
Brian <riceb53@gmail.com>
2018-02-26 21:44:13 -0600

    add employees

---
+++ employees.rb
@@ -0,0 +1,82 @@
+# model employees using ruby
+#
+
+# array
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "Campos", 70000, false]
+
+# Majora Carter makes $80000 per year
+# p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+
+# # hash
+# employee1 = {
+#   "last_name" => "Carter",
+#   "salary" => 80000,
+#   "active" => true,
+#   "first_name" => "Majora"
+# }
+# employee2 = {
+#   "first_name" => "Danilo",
+#   "last_name" => "Campos",
+#   "salary" => 70000,
+#   "active" => false
+# }
+
+
+# employee2 = {
+#   :first_name => "Danilo",
+#   :last_name => "Campos",
+#   :salary => 70000,
+#   :active => false
+# }
+
+# SAME AS employee2 on line 27
+# employee2 = {
+#   first_name: "Danilo",
+#   last_name: "Campos",
+#   salary: 70000,
+#   active: false
+# }
+
+# p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
+
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+
+
+
+class Employee
+  attr_reader :first_name, :last_name, :salary
+  attr_writer :first_name
+
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+
+  # setter/writer
+
+
+
+  def print_info
+    p "#{first_name} #{last_name} makes $#{salary} per year"
+  end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+  end
+end
+
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, false)
+
+# p 'printing employee1.....'
+# p employee1
+p employee1.first_name
+
+employee1.first_name = "joey"
+p employee1.first_name
