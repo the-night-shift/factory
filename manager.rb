2cb4fbae499cf9b8042ea08eadb912cb6744e525
Brian <riceb53@gmail.com>
2018-02-27 21:18:09 -0600

    give annual raises with times loop

--- manager.rb
+++ manager.rb
@@ -21,47 +21,45 @@ class Employee
 end
 
 employee1 = Employee.new({:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true})
-employee1.print_info
+# employee1.print_info
 employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: false)
-employee2.print_info
+# employee2.print_info
 
-
-# p 'printing employee1.....'
-# p employee1
-# p employee1.first_name
-
-# employee1.first_name = "joey"
-# p employee1.first_name
 
 
 class Manager < Employee
-  # attr_reader :first_name, :last_name, :salary
-  # attr_writer :first_name
+  attr_reader :employees
 
-  # def initialize(input_options)
-  #   @first_name = input_options[:first_name]
-  #   @last_name = input_options[:last_name]
-  #   @salary = input_options[:salary]
-  #   @active = input_options[:active]
-  # end
-
-  # def print_info
-  #   p "#{first_name} #{last_name} makes $#{salary} per year"
-  # end
-
-  # def give_annual_raise
-  #   @salary *= 1.05
-  # end
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
+  end
 
   def send_report
     p 'going to send that report...'
     # write some real code in here
     p 'totally just sent that report'
   end
+
+  def give_all_raises
+    p 'totally gonna give everybody a raise rn'
+    i = 0
+    @employees.length.times do
+      @employees[i].give_annual_raise
+      i += 1
+    end
+  end
 end
 
+manager1 = Manager.new({:first_name => "Manny", :last_name => "Williams", :salary => 100000, :active => true, employees: [employee1, employee2]})
 
-manager1 = Manager.new({:first_name => "Manny", :last_name => "Williams", :salary => 100000, :active => true})
+p manager1
+manager1.give_all_raises
+p manager1
+# p manager1.employees
+# Create a method in the Manager class called give_all_raises that loops through each of the manager’s employees and gives them a raise (using the give_annual_raise method).
 
-manager1.print_info
-manager1.send_report
+# make a method called give_all_raises
+# what are the manager's employees?
+# loop through manager's employees
+# give them all a raise (using give_annual_raise)
