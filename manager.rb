ff0ffec178631a4ad79c95072b4c52cb8a84f88f
Brian <riceb53@gmail.com>
2018-02-27 21:21:52 -0600

    give annual raises with each loop

--- manager.rb
+++ manager.rb
@@ -43,10 +43,13 @@ class Manager < Employee
 
   def give_all_raises
     p 'totally gonna give everybody a raise rn'
-    i = 0
-    @employees.length.times do
-      @employees[i].give_annual_raise
-      i += 1
+    # i = 0
+    # @employees.length.times do
+    #   @employees[i].give_annual_raise
+    #   i += 1
+    # end
+    @employees.each do |employee|
+      employee.give_annual_raise
     end
   end
 end
